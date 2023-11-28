8d8ab06d337cb86285e6badadd52c2b5de87e3e7
xVllad44v2 <xvlad44@gmail.com>
2023-11-28 21:39:01 +0200

    first commit

---
+++ dgw cu imagini/dgw cu imagini/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace dgw_cu_imagini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Image";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 5;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(126, 196);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(100, 30);
+            this.btn_load.TabIndex = 6;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(29, 381);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(197, 38);
+            this.btn_add.TabIndex = 7;
+            this.btn_add.Text = "Adauga";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(274, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 303);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(274, 381);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(551, 38);
+            this.btn_del.TabIndex = 9;
+            this.btn_del.Text = "Delete Selections";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 247);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 457);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "DGW IMG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ dgw cu imagini/dgw cu imagini/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dgw_cu_imagini
+{
+    public partial class Form1 : Form
+    {
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\db.mdf;Integrated Security=True;Connect Timeout=30";
+        private bool fetched = false;
+        private List<string> codesToDelete = new List<string>();
+        private bool checkEmpty()
+        {
+            if (textBox2.Text == "" || textBox3.Text == "")
+                return true;
+            return false;
+        }
+        private void clearTextBoxes()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            pictureBox1.Image = null;
+        }
+        private byte[] ConvertImageToBytes()
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(pictureBox1.Image, typeof(byte[]));
+        }
+        private Image convertBinaryToImage(object data)
+        {
+            if (data != DBNull.Value)
+            {
+                ImageConverter converter = new ImageConverter();
+                return (Image)converter.ConvertFrom((byte[])data);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private void fetchData()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string query = "SELECT * FROM Gadgets";
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+
+                dt.Columns.Add("code", typeof(string));
+                dt.Columns.Add("name", typeof(string));
+                dt.Columns.Add("path", typeof(string));
+                dt.Columns.Add("image", typeof(Image));
+                while (reader.Read())
+                {
+                    DataRow row = dt.NewRow();
+                    row["code"] = reader["code"].ToString();
+                    row["name"] = reader["name"].ToString();
+                    row["path"] = reader["path"].ToString();
+                    row["image"] = convertBinaryToImage(reader["image"]);
+                    dt.Rows.Add(row);
+                }
+                con.Close();
+
+                dataGridView1.DataSource = dt;
+
+                DataGridViewImageColumn img = dataGridView1.Columns["image"] as DataGridViewImageColumn;
+                img.ImageLayout = DataGridViewImageCellLayout.Stretch;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            if (!fetched)
+            {
+                DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+                chk.HeaderText = "Delete";
+                chk.Name = "chk";
+                dataGridView1.Columns.Add(chk);
+                fetched = true;
+            }
+        }
+        private void enlargeImage()
+        {
+            DataGridViewImageColumn img = new DataGridViewImageColumn();
+            img = (DataGridViewImageColumn)dataGridView1.Columns["image"];
+            img.ImageLayout = DataGridViewImageCellLayout.Stretch;
+
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Enabled = false;
+            dataGridView1.AllowUserToAddRows = false;
+            fetchData();
+           // enlargeImage();
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            FileDialog fd = new OpenFileDialog();
+            fd.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = fd.FileName;
+                pictureBox1.Image = new Bitmap(fd.FileName);
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (checkEmpty())
+            {
+                MessageBox.Show("Please fill all the fields!");
+                return;
+            }
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string query = "INSERT INTO Gadgets (name, path, image) VALUES (@name, @path, @image)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@path", textBox3.Text);
+                cmd.Parameters.AddWithValue("@image", ConvertImageToBytes());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                fetchData();
+                clearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                MessageBox.Show("An error occured!");
+            }
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(codesToDelete.Count);
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                foreach (string code in codesToDelete)
+                {
+                    string query = "DELETE FROM Gadgets WHERE code = @code";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@code", code);
+                    cmd.ExecuteNonQuery();
+                }
+                con.Close();
+                fetchData();
+                codesToDelete.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                MessageBox.Show("An error occured!");
+            }
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex == -1) return;
+            try
+            {
+                if(e.ColumnIndex == dataGridView1.Columns["chk"].Index)
+                {
+                    if (dataGridView1.Rows[e.RowIndex].Cells["chk"].Value == null)
+                    {
+                        dataGridView1.Rows[e.RowIndex].Cells["chk"].Value = true;
+                        codesToDelete.Add(dataGridView1.Rows[e.RowIndex].Cells["code"].Value.ToString());
+                        Console.WriteLine(codesToDelete.Count);
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[e.RowIndex].Cells["chk"].Value = null;
+                        codesToDelete.Remove(dataGridView1.Rows[e.RowIndex].Cells["code"].Value.ToString());
+                        Console.WriteLine(codesToDelete.Count);
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+               Console.WriteLine(ex);
+            }
+            
+        }
+   
+    }
+}
